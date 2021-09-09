265d0265e1d5b331794888ca3f26dd21775dc971
Sally Poon <sallypoon@hotmail.com>
2021-09-08 23:22:06 -0700

    import dfs helper, complete bfs

--- breadth_first_search/breadth_first_search.py
+++ breadth_first_search/breadth_first_search.py
@@ -1,4 +1,6 @@
 from queue_ds.queue_using_deque import Queue
+from depth_first_search.retrieve_dfs_path import get_path
+
 
 def OFFSETS() -> dict:
     return {
@@ -8,6 +10,7 @@ def OFFSETS() -> dict:
         "up": (-1, 0)
     }
 
+
 def bfs(maze: list, start: tuple, goal: tuple):
     queue = Queue()
     queue.enqueue(start)
@@ -16,4 +19,11 @@ def bfs(maze: list, start: tuple, goal: tuple):
     while queue:
         current = queue.dequeue()
         if current == goal:
-            return
+            return get_path(predecessors, start, goal)
+        for direction in ["up", "right", "down", "left"]:
+            row_offset, column_offset = OFFSETS()[direction]
+            neighbour = (current[0] + row_offset, current[1] + column_offset)
+            if neighbour in maze and neighbour not in predecessors:
+                queue.enqueue(neighbour)
+                predecessors[neighbour] = current
+
