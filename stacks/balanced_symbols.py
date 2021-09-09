c9e84f85fd6675ea20f6e1af96cdab08357a0620
Sally Poon <sallypoon@hotmail.com>
2021-09-08 17:43:47 -0700

    complete problem

--- stacks/balanced_symbols.py
+++ stacks/balanced_symbols.py
@@ -1,27 +1,33 @@
-def balancedBrackets(string: str) -> bool:
-    close_brackets = "})]"
-    symbol_pair = {"(": ")", "{": "}", "[": "]"}
-    stack = []
+"""
+Given a string of bracket symbols, determine if the string is balanced.
+
+"""
+
+import stack
 
 
-class SymbolStack:
+def balanced_brackets(brackets: str) -> bool:
+    """Return boolean if a given string of brackets is balanced.
 
-    def __init__(self):
-        """
-        Instantiate the SymbolStack object.
-        """
-        self.items = []
+    :param brackets: a String
+    :precondition: String contains bracket characters only: "(){}[]"
+    :postcondition: return true if bracket string is balanced, return false if bracket string is not balanced
+    :return: Boolean
+    """
+    open_brackets = "[{("
+    bracket_pair = {"]": "[", "}": "{", ")": "("}
+    given_brackets, evaluate_stack = stack.Stack(), stack.Stack()
+    given_brackets.items = [bracket for bracket in brackets]
+    while not given_brackets.is_empty():
+        if given_brackets.peek() not in open_brackets:
+            evaluate_stack.add(given_brackets.remove())
+        elif given_brackets.peek() == bracket_pair[evaluate_stack.peek()]:
+            given_brackets.remove()
+            evaluate_stack.remove()
+        else:
+            break
+    return len(evaluate_stack.items) == 0
 
-    def add(self, item: str) -> None:
-        pass
 
-    def remove(self):
-        pass
-
-    def peek(self) -> str:
-        pass
-
-    def is_empty(self):
-        pass
-
-
+if __name__ == '__main__':
+    print(balanced_brackets("({}{{}})"))
