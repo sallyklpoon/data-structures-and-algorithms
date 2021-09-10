f7e7ab0f9d24ece79328956e9283a2328b16c5c1
Sally Poon <sallypoon@hotmail.com>
2021-09-09 19:57:47 -0700

    complete single linked list node class

---
+++ linked_lists/sll_node.py
@@ -0,0 +1,56 @@
+"""
+Implementation of a single linked list node.
+"""
+
+
+class SllNode:
+
+    def __init__(self, data):
+        """
+        Instantiate a Node class.
+
+        Done in constant time.
+
+        :param data: any data type
+        :postcondition: instantiate a Node object
+        """
+        self.data = data
+        self.next = None
+
+    def get_data(self):
+        """
+        Return the data of the node.
+
+        Done in constant time
+
+        :return: the data of the node
+        """
+        return self.data
+
+    def set_data(self, new_data):
+        """
+        Update the node's data.
+
+        Done in constant time
+
+        :param new_data: a new data type to override current data
+        :return: None, the node's data is updated
+        """
+        self.data = new_data
+
+    def get_next(self):
+        """
+        Return the next node attribute.
+
+        :return: the next node object
+        """
+        return self.next
+
+    def set_next(self, new_next):
+        """
+        Replace the existing value of the self.next attribute with new_next
+
+        :param new_next: a Node object
+        :return: None, the current node with self.next updated
+        """
+        self.next = new_next
