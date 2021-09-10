16f92b599203751d48c4c2baea8046769e16be17
Sally Poon <sallypoon@hotmail.com>
2021-09-09 21:21:38 -0700

    DEFINE some sll methods

---
+++ linked_lists/sll.py
@@ -0,0 +1,30 @@
+"""
+Implement single linked-list class.
+"""
+
+
+class Sll:
+
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        return f"SLL object: head={self.head}"
+
+    def is_empty(self):
+        pass
+
+    def add_front(self):
+        pass
+
+    def add_rear(self):
+        pass
+
+    def size(self):
+        pass
+
+    def search(self, data):
+        pass
+
+    def remove(self, data):
+        pass
